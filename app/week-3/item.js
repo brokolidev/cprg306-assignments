4d11bd2ca5524cd01de048feeddae7a8b933e8d6
Hyunseung Choi <bocalist@gmail.com>
2024-09-19 13:26:09 -0600

    changed styling a little

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,13 +1,10 @@
-
 export default function Item({name, quantity, category}) {
     return (
         <ul className="border-gray-500 border
-        bg-amber-200 inline-block
-        text-amber-800
-        max-w-96 m-5 p-4 min-h-32">
-            <li>Item Name: {name}</li>
-            <li>Quantity: {quantity}</li>
-            <li>Category: {category}</li>
+            bg-amber-200 text-amber-800
+            max-w-96 m-5 p-4">
+            <li className="text-lg capitalize font-bold">{name}</li>
+            <li>Buy {quantity} in {category}</li>
         </ul>
     );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -8,11 +8,11 @@ export const metadata = {
 export default function Page() {
 
     return (
-        <main>
+        <main className="bg-amber-50">
             <Link href="/" className="p-5 inline-block">
                 &#8592; Go Back
             </Link>
-            <h1 className="font-sans text-3xl font-extrabold p-5 mb-5">
+            <h1 className="font-sans text-3xl font-extrabold px-5 mb-5">
                 Shopping List
             </h1>
             <div className="text-center">
