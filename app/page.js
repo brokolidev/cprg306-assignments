1be93c57a3f242466ae85565cdca54ac07403401
brokolidev <bocalist@gmail.com>
2024-10-14 10:25:13 -0600

    added week5 link

--- app/page.js
+++ app/page.js
@@ -23,6 +23,9 @@ export default function Home() {
             <li className={linkStyles}>
                 <Link href="/week-4">Week 4</Link>
             </li>
+            <li className={linkStyles}>
+                <Link href="/week-5">Week 5</Link>
+            </li>
         </ul>
     </main>
   );
